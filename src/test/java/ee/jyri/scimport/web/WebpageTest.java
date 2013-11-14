376e87ce57e6f51a0a1a68392082a862ee10e450
Jyri Andrejev <jyriand@gmail.com>
2013-11-14 19:22:56 +0200

    Refactored constant

--- src/test/java/ee/jyri/scimport/web/WebpageTest.java
+++ src/test/java/ee/jyri/scimport/web/WebpageTest.java
@@ -1,6 +1,5 @@
 package ee.jyri.scimport.web;
 
-import com.sun.xml.internal.ws.api.pipe.ContentType;
 import ee.jyri.scimport.domain.Song;
 import ee.jyri.scimport.service.SoundcloudService;
 import ee.jyri.scimport.service.UploadService;
@@ -10,8 +9,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -31,9 +28,9 @@ public class WebpageTest {
 
     private MockMvc mockMvc;
 
-    public static final String VALID_USERNAME = "ValidUser";
+    public static final String EXISTING_USER = "ValidUser";
 
-    public static final String INVALID_USERNAME = "InValidUser";
+    public static final String NONEXISTING_USER = "InValidUser";
 
     public static final String JSON_CHARSET = "application/json;charset=UTF-8";
 
@@ -61,7 +58,7 @@ public class WebpageTest {
     }
 
     @Test
-    public void searchForExistngUsersSongs_shouldReturnModelWithSongs() throws Exception {
+    public void searchForExistngUsersSongs_returnModelWithSongs() throws Exception {
         List<Song> songs = new ArrayList<Song>();
         songs.add( new Song("1"));
         songs.add( new Song("2"));
@@ -69,22 +66,22 @@ public class WebpageTest {
         when(soundcloudService.findUserSongs(anyString()))
                 .thenReturn(songs);
 
-        mockMvc.perform(get("/searchUser/" + VALID_USERNAME))
+        mockMvc.perform(get("/searchUser/" + EXISTING_USER))
                 .andExpect(model().attributeExists(MODEL_SONGS))
                 .andExpect(model().attribute(MODEL_SONGS, songs));
 
-        verify(soundcloudService, times(1)).findUserSongs(VALID_USERNAME);
+        verify(soundcloudService, times(1)).findUserSongs(EXISTING_USER);
     }
 
     @Test
-    public void searchForNonExistingUsersSongs_shouldReturnNothing() throws Exception {
+    public void searchForNonExistingUsersSongs_returnsNothing() throws Exception {
         ArrayList<Song> emptyList = new ArrayList<Song>();
         when(soundcloudService.findUserSongs(anyString())).thenReturn(emptyList);
 
-        mockMvc.perform(get("/searchUser/" + INVALID_USERNAME))
+        mockMvc.perform(get("/searchUser/" + NONEXISTING_USER))
                 .andExpect(model().attribute(MODEL_SONGS, emptyList));
 
-        verify(soundcloudService, times(1)).findUserSongs(INVALID_USERNAME);
+        verify(soundcloudService, times(1)).findUserSongs(NONEXISTING_USER);
     }
 
     @Test
@@ -95,11 +92,11 @@ public class WebpageTest {
 
         when(soundcloudService.findUserSongs(anyString())).thenReturn(songs);
 
-        mockMvc.perform(get("/" + VALID_USERNAME + "/saveSong/1"))
+        mockMvc.perform(get("/" + EXISTING_USER + "/saveSong/1"))
             .andExpect(content().contentType(JSON_CHARSET))
             .andExpect(jsonPath("$.success").value(true));
 
-        verify(soundcloudService, times(1)).findUserSongs(VALID_USERNAME);
+        verify(soundcloudService, times(1)).findUserSongs(EXISTING_USER);
         verify(uploadService).uploadFile(any( Song.class ));
 
     }
@@ -108,11 +105,11 @@ public class WebpageTest {
     public void saveSongToStorage_withNonExistingSong_returnsFailure() throws Exception {
         when(soundcloudService.findUserSongs(anyString())).thenReturn( new ArrayList<Song>());
 
-        mockMvc.perform(get("/" + VALID_USERNAME + "/saveSong/1"))
+        mockMvc.perform(get("/" + EXISTING_USER + "/saveSong/1"))
                 .andExpect((content().contentType(JSON_CHARSET)))
                 .andExpect(jsonPath("$.success").value(false));
 
-        verify(soundcloudService, times(1)).findUserSongs(VALID_USERNAME);
+        verify(soundcloudService, times(1)).findUserSongs(EXISTING_USER);
         verifyZeroInteractions(uploadService);
     }
 }
