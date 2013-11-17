51981f772e81169d2e0a40f019e0a08c986fb103
Jyri Andrejev <jyriand@gmail.com>
2013-11-17 19:46:41 +0200

    Added bucketName

--- src/main/java/ee/jyri/scimport/service/AwsApiAdapter.java
+++ src/main/java/ee/jyri/scimport/service/AwsApiAdapter.java
@@ -2,11 +2,15 @@ package ee.jyri.scimport.service;
 
 
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AwsApiAdapter
 {
+
+    @Value("${s3.bucketName}")
+    private String bucketName;
 
     public void upload( String url, String key, ObjectMetadata metaData )
     {
--- src/test/java/ee/jyri/scimport/ee/jyri/scimport/integration/IntegrationTest.java
+++ src/test/java/ee/jyri/scimport/ee/jyri/scimport/integration/IntegrationTest.java
@@ -1,30 +1,53 @@
 package ee.jyri.scimport.ee.jyri.scimport.integration;
 
 import ee.jyri.scimport.domain.Track;
+import ee.jyri.scimport.service.AwsApiAdapter;
+import ee.jyri.scimport.service.S3TrackUploadService;
 import ee.jyri.scimport.service.SoundcloudTrackService;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Map;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
-public class SoundcloudIntegrationTest
+public class IntegrationTest
 {
     @Autowired
     private SoundcloudTrackService trackService;
 
+    @Autowired
+    private S3TrackUploadService uploadService;
+
+    @Autowired
+    private AwsApiAdapter awsApiAdapter;
+
+    @Value("${s3.bucketName}")
+    private String bucketName;
+
+
     @Test
-    @Ignore
-    public void testName() throws Exception {
+    public void soundcloudAdapterCanFetchUserTracksJson() throws Exception {
         Map<String,Track> tracks = trackService.findUserTracks("apitest");
         assertThat(tracks.size(), greaterThan(1));
+    }
+
+    @Test
+    public void awsApiAdapterHasCorrectProperties() throws Exception {
+
+        String bucket = (String) ReflectionTestUtils.getField(awsApiAdapter, "bucketName");
+        assertThat(bucket, notNullValue());
+        assertThat( bucket, is(bucketName));
     }
 }
